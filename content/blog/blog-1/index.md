b99fe57bb6d4f1c376c00c49f8eaddf76823af91
derrmru <podogomacbookair@MacBook-Air.local>
2020-08-29 18:47:53 +0200

    Blog Post Template

--- content/blog/blog-1/index.md
+++ content/blog/blog-1/index.md
@@ -1,7 +1,17 @@
 ---
 type: blog
+date: 2020-08-27T12:22:43.109Z
 title: 'First Blog'
 slug: '/blog/first-blog'
+image: './image.png'
 ---
 
-This is the first blog entry
+This is the first blog entry.
+
+No other entry is anywhere near as saucy as this entry.
+
+Clothing is good.
+
+Also, loud clothing is good.
+
+I like colour and stuff.
--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -1,11 +1,63 @@
 import React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/layout"
+import Img from "gatsby-image"
 
-const BlogTemplate = () => {
+import styles from "./blogTemplate.module.css"
+
+const BlogTemplate = ({ data }) => {
+    const { markdownRemark } = data 
+    const { frontmatter, html } = markdownRemark
+
     return (
-        <div>
-            Blog
-        </div>
+        <Layout>
+            <div className={styles.btb}>
+                <a href="/blog">|Back To Blog</a>
+            </div>
+
+            <div className={styles.titleContainer}>
+                <h1>{frontmatter.title}</h1>
+                <h2>{frontmatter.date}</h2>
+            </div>
+
+            <hr />
+
+            <div className={styles.blogContainer}>
+
+                <div className={styles.blogText}
+                    dangerouslySetInnerHTML={{ __html: html }}
+                    />
+                
+                <div className={styles.blogImage}>
+                    <Img fluid={frontmatter.image.childImageSharp.fluid} />
+                </div>
+            
+            </div>
+
+            <hr />
+
+        </Layout>
     )
 }
+
+export const blogQuery = graphql`
+  query($slug: String!) {
+    markdownRemark(frontmatter: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        date(formatString: "MMMM DD, YYYY")
+        slug
+        title
+        image {
+            childImageSharp {
+              fluid {
+                ...GatsbyImageSharpFluid
+              }
+            }
+        }
+      }
+    }
+  }
+`
 
 export default BlogTemplate
