1b4a3cae603a0a5aa0796ed13f64df6e027804ef
derrmru <podogomacbookair@MacBook-Air.local>
2020-11-04 17:07:08 +0100

    Full Instagram Feed Placeholder

--- src/components/instagramFeed.js
+++ src/components/instagramFeed.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from "react"
-import image1 from "../images/nfe-background.png"
-import image2 from "../images/contact-background.png"
+import image1 from "../images/instagram-1.png"
+import image2 from "../images/instagram-2.png"
 import styles from "./instagramFeed.module.css"
 
-let InstagramFeed = (props) => {
+let InstagramFeed = () => {
     const iArr = [
-        [image1, "#youwearitbest #loudfashion #otherhashtags"], 
-        [image2, "#material #happy #colourful"]
+        [image1, <><p>Two of a kind</p> <p>#onewayoranother #wearitifyoucan #courtainstowear</p></>], 
+        [image2, <><p>Just a couple of fabrics I found in charity shops.</p><p>Ready to give them a second chance.</p><p>#rebuild #onemorechance #bargain</p></>]
     ]
     const [currentImage, setCurrentImage] = useState(iArr[0][0])
     const [currentHash, setCurrentHash] = useState(iArr[0][1])
@@ -22,14 +22,33 @@ let InstagramFeed = (props) => {
         return () => clearInterval(interval);
     })
 
+    //instagram logo and link
+    const [mouse, setMouse] = useState(false);
+
     return (
         <div className={styles.iFeedContainer}>
-            <div className={styles.iFImage}>
-                <img src={currentImage} alt="" />
+            <div 
+                className={styles.iFImage} 
+                onMouseOver={() => setMouse(true)}
+                onFocus={() => setMouse(true)}
+                onMouseLeave={() => setMouse(false)}
+                >
+                <a href="https://www.instagram.com/you.wearitbest/" target="_blank" rel="noreferrer">
+                    {
+                        mouse && <div className={styles.instImage}>
+                            <div className={styles.instLogo}>
+                                <div className={styles.instInnerSquare}></div>
+                                <div className={styles.instMiddleCircle}></div>
+                                <div className={styles.instTopCircle}></div>
+                            </div>
+                        </div>
+                    }
+                    <img src={currentImage} alt="" />
+                </a>
             </div>
 
             <div className={styles.iFhash}>
-                <p>{currentHash}</p>
+                <div className={styles.theHash}>{currentHash}</div>
             </div>
 
         </div>
